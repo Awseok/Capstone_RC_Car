e040c0b99ed39af7c08ed8bc8cd45f69754e6ad9
An wonseok <nave0048@naver.com>
2019-11-16 21:04:33 +0900

    예외처리 수정(테스트 필요!)
    테스트 필요

--- car.py
+++ car.py
@@ -1,98 +1,187 @@
-from socket import *
-from time import sleep
-import RPi.GPIO as GPIO, os, sys
+#-*- coding: utf-8 -*-
+"""
+Created on Sat Nov 16 15:05:25 2019
 
-def run():
-    HIGH = 1
-    LOW = 0
-    cnt1 = 50
-    cnt2 = 50
-    close = False
+@author: wonseok
 
-    leftMotor = [19,26]
-    rightMotor = [20,21]
-    enableButton = [6, 12]
-    
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(leftMotor[0], GPIO.OUT)
-    GPIO.setup(leftMotor[1], GPIO.OUT)
-    GPIO.setup(rightMotor[0], GPIO.OUT)
-    GPIO.setup(rightMotor[1], GPIO.OUT)
+need test
+"""
+import RPi.GPIO as GPIO, sys, socket, threading, time
 
-    GPIO.output(leftMotor[0], LOW)
-    GPIO.output(leftMotor[1], LOW)
-    GPIO.output(rightMotor[0], LOW)
-    GPIO.output(rightMotor[1], LOW)
-
-    GPIO.setup(enableButton[0], GPIO.OUT)
-    GPIO.setup(enableButton[1], GPIO.OUT)
-
-    pwm1 = GPIO.PWM(enableButton[0], cnt1)
-    pwm2 = GPIO.PWM(enableButton[1], cnt2)
-
-    pwm1.start(cnt1)
-    pwm2.start(cnt2)    
-
-    ip = '192.168.43.249'
-    #ip = input("ip: ")
+#thread - check that client socket connection is alive
+def checkConnect(self):
+    '''
+    Every 30 second, server send connect check value 'C'
+    '''
+    while True:
+        #current execute time
+        startTime = time.perf_count()
+            
+        checkMsg = "C"
+        connectCheck = connectionSocket.send(checkMsg.decode('utf-8'))
+        #client socket send closed
+        if connectCheck == b'':
+            self.isClose = True
         
-    port = 54321
-    #port = input("port: ")
+        '''
+        wait 30 second
+        for client send return value 'C'
+        ''' 
+        checkTime = time.perf_count()
+        while True:
+            if time.perf_count() - checkTime > 30:
+                self.isClose = True
+                break
+            if cntlMsg == 'C':
+                break
+        
+        while time.perf_count() - startTime < 30:
+            pass
+        
+#pin output value
+HIGH = 1
+LOW = 0
 
-    addr = (ip, int(port))
+#flag for close
+isClose = False
 
-    serverSocket = socket(AF_INET, SOCK_STREAM)
+#pwm setting(speed, pin)
+motor1speed = 50
+motor2speed = 50
+leftMotor = [19,26]
+rightMotor = [20,21]
+enableButton = [6, 12]
+
+#GPIO motor pin setting
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(leftMotor[0], GPIO.OUT)
+GPIO.setup(leftMotor[1], GPIO.OUT)
+GPIO.setup(rightMotor[0], GPIO.OUT)
+GPIO.setup(rightMotor[1], GPIO.OUT)
+
+#GPIO first value
+GPIO.output(leftMotor[0], LOW)
+GPIO.output(leftMotor[1], LOW)
+GPIO.output(rightMotor[0], LOW)
+GPIO.output(rightMotor[1], LOW)
+
+#GPIO pwm pin setting
+GPIO.setup(enableButton[0], GPIO.OUT)
+GPIO.setup(enableButton[1], GPIO.OUT)
+
+#pwm bind
+pwm1 = GPIO.PWM(enableButton[0], motor1speed)
+pwm2 = GPIO.PWM(enableButton[1], motor2speed)
+
+#pwm start
+pwm1.start(motor1speed)
+pwm2.start(motor2speed)    
     
+#ip, port
+#ip = input("ip: ")
+ip = '192.168.43.249'
+
+#port = input("port: ")
+port = 54321
+        
+addr = (ip, int(port))
+ 
+print("ip, port :" + addr)
+
+#bind Server socket
+while True:
     try:
+        serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("Make server socket")
+        
         serverSocket.bind(addr)
+        print("binded socket")
+        break
+    
     except:
         print("reconnect")
-        sleep(2)
+        time.sleep(2)
         serverSocket.close()
-        os.execl(sys.executable, *sys.argv)
-        
-    serverSocket.listen(1)
-    connectionSocket, connectAddr = serverSocket.accept();
+        continue
     
-    print("connect")
-
+    #waiting for client socket(1)    
+    serverSocket.listen(1)
+    
+    #get client socket
+    connectionSocket, connectAddr = serverSocket.accept();
+           
+    print("connect")   
+    
+    threading._start_new_thread(target=checkConnect, args=())
+    #comunication    
     while True:
+                
+        #motor output flag (motor1 in1, motor1 in2, motor2 in1, motor2 in2)
         cntlFlag = [LOW, LOW, LOW, LOW]
         
+        #recive
         try:
             cntlMsg = connectionSocket.recv(1)
+            
+        #can't handle IOException
         except:
-            close()
+            isClose = True
+            
         finally:
+            #if client send socket close
             if cntlMsg == b'':
-                close()
+                isClose = True
+            
+            #socket error
             elif cntlMsg == -1:
-                close()
+                isClose = True
+                
+        #close
+        if isClose:
+            connectionSocket.close()
+            serverSocket.close()
+            pwm1.stop()
+            pwm2.stop()
+            GPIO.cleanup()
+            sys.exit()
             
         cntlMsg = cntlMsg.decode('utf-8')
        
+        #close
         if cntlMsg == 'E':
-            close = True
+            isClose = True
+        #newline charator
         elif cntlMsg =='\n':
             continue
+        #left
         elif cntlMsg == 'L':
             cntlFlag = [LOW, LOW, HIGH, LOW]
+        #Right
         elif cntlMsg == 'R':
             cntlFlag = [HIGH, LOW, LOW, LOW]
+        #Foward
         elif cntlMsg == 'F':
             cntlFlag = [HIGH, LOW, HIGH, LOW]
+        #Nuetral
         elif cntlMsg == 'N':
             cntlFlag = [LOW, LOW, LOW, LOW]
+        #Back
         elif cntlMsg == 'B':
             cntlFlag = [LOW, HIGH, LOW, HIGH]
-        else:                
-            cnt1 = int(cntlMsg)
-            cnt2 = int(cntlMsg)   
-                                
-        pwm1.ChangeDutyCycle(cnt1)
-        pwm2.ChangeDutyCycle(cnt2)
-
+        #speed value
+        elif cntlMsg.isdecimal():               
+            motor1speed = int(cntlMsg)
+            motor2speed = int(cntlMsg)
+        #exception
+        else:
+            print(cntlMsg + "is garbage")
+        
+        #change motor speed
+        pwm1.ChangeDutyCycle(motor1speed)
+        pwm2.ChangeDutyCycle(motor2speed)
+        
+        #motor[in1, in2]
         leftMotorFlag = cntlFlag[0:2]
         rightMotorFlag = cntlFlag[2:4]    
 
@@ -100,20 +189,4 @@ def run():
         GPIO.output(leftMotor[1], leftMotorFlag[1])
         GPIO.output(rightMotor[0], rightMotorFlag[0])
         GPIO.output(rightMotor[1], rightMotorFlag[1])
-
-
-        if close == True:
-            connectionSocket.close()
-            serverSocket.close()
-            pwm1.stop()
-            pwm2.stop()
-            GPIO.cleanup()
-            sys.exit()
-    
-if __name__ == "__maiin__":    
-    run()
-
-run()
-    
-
     
